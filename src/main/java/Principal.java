727d7742d414ce8aab31a7c5e963a23e9238d37b
cainpaiva <you@example.comcaiopaivaspfc5@gmail.com>
2024-10-21 16:48:43 -0300

    Implementação de Record e aplicação dos conceitos de deep imutability e shallow imutability.

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,9 +1,24 @@
+import exercicios.Cidade;
+import exercicios.CidadeRecord;
+import exercicios.PessoaRecord;
+import exercicios.PessoaRecordShallow;
+
 /**
  * Classe para você testar suas implementações.
  * Veja o README para mais detalhes.
  */
 public class Principal {
     public static void main(final String[] args) {
+        final var cidade1 = new Cidade("Pium");
+        final var cidade2 = new CidadeRecord("Pugmil");
 
+        final var pessoa = new PessoaRecordShallow("Julio", cidade1);
+        System.out.println(pessoa);// nome: Julio, Cidade: Pium
+        cidade1.setNome("Palmas"); // alterando o nome da cidade provando que a classe Pessoa... é superficialmente imutavel Shallow Immutability
+        System.out.println(pessoa);//nome: Julio, Cidade: Palmas
+
+        final var pessoaImutavel = new PessoaRecord("Manoel", cidade2);
+        System.out.println("\n"+pessoaImutavel);//nome: Manoel, Cidade: Pugmil
+        System.out.println("PessoaRecord profundamente (totalmente) imutável (deep imutability), pois o Record: CidadeRecord não possui metodos para alteração de valor da variavel nome");
     }
 }
